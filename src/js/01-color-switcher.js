6a9ceb4373779f1610f9f331562ed51bc40d04da
Ilya Bondarchuk <cod3provider@gmail.com>
2023-11-10 12:47:40 +0200

    first task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+import getRandomHexColor from './getRandomHexColor.js';
 
+const elements = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  bodyStyle: document.body.style,
+}
+
+let timerId = null;
+
+const handleStart = () => {
+  timerId = setInterval(() => elements.bodyStyle.backgroundColor = getRandomHexColor(), 1000);
+
+  if (timerId) {
+    elements.startBtn.setAttribute('disabled', '');
+  }
+}
+
+const handleStop = () => {
+  clearInterval(timerId);
+  elements.startBtn.removeAttribute('disabled');
+}
+
+elements.startBtn.addEventListener('click', handleStart);
+elements.stopBtn.addEventListener('click', handleStop);
