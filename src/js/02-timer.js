ceb165471fa60639353c7fd50e6a2fc4edb87380
Ilya Bondarchuk <cod3provider@gmail.com>
2023-11-12 18:26:46 +0200

    second task: main features done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,16 +1,19 @@
 import flatpickr from 'flatpickr';
-
 import 'flatpickr/dist/flatpickr.min.css';
-
 
 const elements = {
   startBtn: document.querySelector('button[data-start]'),
-  input: document.querySelector('#datetime-picker'),
-  days: document.querySelector('span[data-days]'),
-  hours: document.querySelector('span[data-hours]'),
-  minutes: document.querySelector('span[data-minutes]'),
-  seconds: document.querySelector('span[data-seconds]'),
+  datePicker: document.querySelector('#datetime-picker'),
+  daysSpan: document.querySelector('span[data-days]'),
+  hoursSpan: document.querySelector('span[data-hours]'),
+  minutesSpan: document.querySelector('span[data-minutes]'),
+  secondsSpan: document.querySelector('span[data-seconds]'),
 };
+
+let startTimer = 0;
+let timeLeft = 0;
+
+// const getDateForTimer = date => new Date(date);
 
 const options = {
   enableTime: true,
@@ -18,14 +21,61 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    startTimer = selectedDates[0];
+    if (startTimer < Date.now()) {
+      window.alert('err')
+      return;
+    }
+    elements.startBtn.removeAttribute('disabled');
+    elements.startBtn.addEventListener('click', handleClick);
   },
 };
 
-const datePicker = flatpickr(elements.input, options);
+flatpickr(elements.datePicker, options);
 
-const handleClick = () => {
+function handleClick () {
+  elements.startBtn.setAttribute('disabled', '');
+  elements.datePicker.setAttribute('disabled', '');
 
+  const timerId = setInterval(() => {
+    const currentTime = Date.now();
+    timeLeft = startTimer - currentTime;
+
+    if (timeLeft <= 1000) {
+      clearInterval(timerId);
+      elements.datePicker.removeAttribute('disabled');
+      return;
+    }
+
+    // const time = convertMs(timeLeft);
+    const {days, hours, minutes, seconds} = convertMs(timeLeft);
+
+    elements.daysSpan.textContent = days;
+    elements.hoursSpan.textContent = hours;
+    elements.minutesSpan.textContent = minutes;
+    elements.secondsSpan.textContent = seconds;
+
+    if(days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0) {
+      clearInterval(timerId);
+      elements.datePicker.removeAttribute('disabled');
+    }
+  }, 1000)
+};
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
 }
 
-elements.startBtn.addEventListener('click', handleClick);
+function addLeadingZero(value) {
+  return String(value).padStart(2, "0");
+}
